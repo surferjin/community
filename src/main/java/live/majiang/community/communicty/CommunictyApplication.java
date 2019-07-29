54a10d161213a018e325bc89b81ab549d4f264a8
surferjin <173533755@qq.com>
2019-07-29 18:46:18 +0800

    first commit

---
+++ src/main/java/live/majiang/community/communicty/CommunictyApplication.java
@@ -0,0 +1,13 @@
+package live.majiang.community.communicty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunictyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunictyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/live/majiang/community/communicty/CommunictyApplicationTests.java
@@ -0,0 +1,16 @@
+package live.majiang.community.communicty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommunictyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
